6346aad5534327deeb0378c9ea029835a91115ce
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 03:19:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
+  
   #your code here
   contacts.each do |person, data|
     puts "#{person}: #{data}"
