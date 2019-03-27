e1230e14015928776f0bb855b43313e6d8f1efef
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 03:28:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,12 +15,12 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"].each do |attribute, value|
-
-    if attribute == :favorite_icecream_flavors
-      value.delete_if do |flavor|
-        puts "strawberry"
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+        flavor == "strawberry"
       end
     end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
